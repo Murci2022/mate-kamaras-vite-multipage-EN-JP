b7cd5e4b49297c04cfa4c177aa6f8855a191166f
MK <Javascript@mates-air.localdomain>
2024-04-21 09:52:30 +0200

    css correction language-selector element in navbar component

--- src/components/Navbar/LanguageSelector.jsx
+++ src/components/Navbar/LanguageSelector.jsx
@@ -14,6 +14,7 @@ const LanguageSelector = ({ selectedLanguage, onChange }) => {
       <select
         onChange={handleLanguageChange}
         defaultValue={selectedLanguage}
+        className='languageSelector'
       >
         <option value='en'>
           <p className='p__cormorant'>ENGLISH</p>
